a54ca6856bc084a7d669a523bb4e1c5badd2f89f
Arraktur <arrakturil@outlook.com>
2022-06-29 03:53:29 +0300

    some commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from '../src/components/App';
 import reportWebVitals from './reportWebVitals';
@@ -9,11 +9,9 @@ const root = ReactDOMClient.createRoot(container);
 
 // Initial render: Render an element to the root.
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App tab="home" />
-    </BrowserRouter>
-  </StrictMode>
+  <HashRouter>
+    <App tab="home" />
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
